0d8a1099a0d76fc87ea0bd2947c810defc11a4e8
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-01-13 10:18:55 +0100

    Set up file structure && styles

---
+++ src/app/layout.jsx
@@ -0,0 +1,15 @@
+import { poppins } from "@/utils/fonts";
+
+export const metadata = {
+  title: "GameSpace | Web's most advanced games library",
+  description:
+    "Internet's most advanced game library to date. Find the game you are looking for and get all the most important infomation about in in just few click!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={poppins.variable}>
+      <body>{children}</body>
+    </html>
+  );
+}
