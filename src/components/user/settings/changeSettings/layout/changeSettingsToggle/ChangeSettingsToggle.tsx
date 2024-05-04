dfe1574ab7ba826c290cb37bb27dcc58ef40657f
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-05-04 09:34:33 +0200

    fix: fix minor importing bugs

--- src/components/user/settings/changeSettings/layout/changeSettingsToggle/ChangeSettingsToggle.tsx
+++ src/components/user/settings/changeSettings/layout/changeSettingsToggle/ChangeSettingsToggle.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import { useUser } from "../../../../../../contexts/userContext/UserContext";
+import { useUserModalStates } from "../../../../../../contexts/userModalStatesContext/UserModalStatesContext";
 import { UserSettingsProvider } from "../../../../../../contexts/userSettingsContext/UserSettingsContext";
 import ChangeSettingsPopup from "../changeSettingPopup/ChangeSettingsPopup";
 
 function ChangeSettingsToggle() {
-  const { state } = useUser();
+  const { inSettingsView } = useUserModalStates();
 
-  const { areSettingsOpen } = state;
-
-  if (!areSettingsOpen) return null;
+  if (!inSettingsView) return null;
   return (
     <UserSettingsProvider>
       <ChangeSettingsPopup />
